463a56a7b6be577f892d9ed13466e4582ab99896
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-11-09 11:45:14 -0600

    commit

---
+++ Parcial2Cajero/Parcial2Cajero/Controllers/CajeroController.cs
@@ -0,0 +1,45 @@
+﻿using Parcial2Cajero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial2Cajero.Controllers
+{
+    public class CajeroController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index1(ClsCajero c)
+        {
+            _ = c;
+
+            if (c.cantidad % 5 == 0)
+            {
+                return Redirect("Resultado");
+            }
+            else
+            {
+                return Redirect("Error");
+            }
+
+        }
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
+        
+
---
+++ Parcial2Cajero/Parcial2Cajero/Models/ClsCajero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2Cajero.Models
+{
+    public class ClsCajero
+    {
+        public int cantidad { get; set; }
+    }
+}
